7b5f1b8af85129a409cb71b417783f8ca7f304f6
RomanSamohwal <rsamohwal@gmail.com>
2020-09-30 04:46:35 +0300

    Updates

---
+++ precache-manifest.9c3c4f92c337a12b633d573e629e3597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35261193ffc7b7c3c03419c28f79156d",
+    "url": "/Todolist_my/index.html"
+  },
+  {
+    "revision": "7a662622b9de5724ea90",
+    "url": "/Todolist_my/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "bfefdc2d841471a63525",
+    "url": "/Todolist_my/static/js/2.4d72d1ce.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist_my/static/js/2.4d72d1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a662622b9de5724ea90",
+    "url": "/Todolist_my/static/js/main.5ec6eb42.chunk.js"
+  },
+  {
+    "revision": "6ee3a25a3e74837460f8",
+    "url": "/Todolist_my/static/js/runtime-main.e05e2c6d.js"
+  }
+]);
